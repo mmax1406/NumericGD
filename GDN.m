c04642c10363f0660167933c5f92eeaf6dad0010
Max <medmaxim96@gmail.com>
2024-03-08 16:49:10 +0200

    Update the code and add a better explanation PDF

--- GDN.m
+++ GDN.m
@@ -1,98 +1,81 @@
-function [sol, delta] = GDN(f, x0, amp, epsilon, maxTime, seeds, range)
-    %---------------------------- GD-Numeric ----------------------------%
-    % Perform gradient descent on any function/simulation which you can 
-    % evaluate but not necessarily differentiate
-    
-    % INPUT : 
-    % OUTPUT :
-    
-    %---------------------------- INIT ----------------------------%
-    % Manipulations on inputs
-    switch nargin
-        case 2
-          amp = 1;
-          seeds = 1;
-          maxTime = inf;
-          epsilon = 1e-10;
-        case 3
-          seeds = 1;
-          maxTime = inf;
-          epsilon = 1e-10;
-        case 4
-          maxTime = inf;
-          seeds = 1;
-        case 5
-          seeds = 1;
-        case 6 
-          range = 10000;
-    end
-    
-    % Check for errors (TBD)
-    err = false;
-%     error(msg)
+function [sol] = GDN(f, limitsTest, x0, amplitude, epsilon)
 
-    %---------------------------- CODE ----------------------------%
+% Protections on dimensions
+if size(x0,2)==1
+    x0 = x0';
+end
+
+% Get the possible gradient directions
+directions = generateNodeCombinations(size(x0,2));
+directions = directions(1:end-1,:)./vecnorm(directions(1:end-1,:),2,2); % The last one is the point itself
+combinations = x0 + amplitude*directions;
+
+% Init the first direction
+bestDirIndex = findBestDir(f, combinations, x0, limitsTest);
+
+while (abs(f(x0 + amplitude*directions(bestDirIndex,:)) - f(x0)) > epsilon) 
     
-    % Create the indices of every poosible direction of the gradients this
-    % is done by giving a significant number to each possible direction and
-    % then creating each combinations wheres each place in each coloumn should 
-    % be in a group of three (belonging to that column)
-    dimVec = 1:size(x0,2)*3;
-    C = nchoosek(dimVec,size(x0,2));
-    for ii=3:3:size(dimVec,2)
-        C = C(C(:,ii/3) <= ii & C(:,ii/3) >= ii-2, :);
-    end
-   
-    % Create seeds
-    if seeds == 1
-        seed_options = x0';
+    if(f(x0 + amplitude*directions(bestDirIndex,:)) < f(x0))
+        x0 = x0 + amplitude*directions(bestDirIndex,:);
     else
-        % range for random seeds (Maybe allow change from outside)
-        seed_options = randn(size(x0,2), seeds)*range;
-    end
-    
-    ii = 1;
-    for x0 = seed_options
-        overall_delta = inf;
-        step_delta = inf;
-        grad = 2*amp;
-        x0 = x0';
-        x = x0;
-        tic; % Take current time
-        % Start optmization either by error or by time
-        while (overall_delta>epsilon && toc<maxTime)% Stop when the change is smaller than epsilon
-            if all(x == x0) || (f(x)-f(x0)) > 0 % First case is on start and if the value isnt smaller
-                grad = 0.5 * grad;
-                x = x0;
-
-                % Find the local grad
-                dx = repelem(x,3) .* repmat([0.99 1 1.01],1,size(x,2));
-                step_delta = inf;
-                for comb = C'
-                    if dx(comb) == x0
-                        continue
-                    else
-                        step = f(dx(comb))-f(x);
-                        if step<step_delta
-                            minima_point = dx(comb);
-                            step_delta = step;
-                        end
-                    end
-                end
-                dir = (minima_point-x)/vecnorm(minima_point-x); % Calculate the new dir
-            else 
-                x0 = x;
-            end 
-            % Update the params for the next loop       
-            x = x0 + grad*dir;
-            overall_delta = abs(f(x));
+        temp = findBestDir(f, x0 + amplitude*directions, x0, limitsTest);
+        if isnan(temp)
+            break % Exit if no direction was found
         end
-
-        % Save for output
-        delta(ii) = f(x);
-        sol(ii,:) = x;
-        ii = ii + 1;
+        % If vectors are parallel than half the amplitude size
+        if all(directions(temp,:) == directions(bestDirIndex,:))...
+                || all(directions(temp,:) == -1 * directions(bestDirIndex,:))
+            amplitude = amplitude/2;
+        end
+        bestDirIndex = temp;
     end
+    if isnan(temp)
+        break % Exit if no direction was found
+    end
+end
+
+sol = x0;
+
+function bestDirIndex = findBestDir(f, combinations, x0, limitsTest)
+    % Testing whats the best gradient direction
+    cost = inf;
+    f0 = f(x0);
+    
+    for ii=1:size(combinations,1)
+        if limitsTest(combinations(ii,:))
+            if f(combinations(ii,:))-f0 < cost
+                cost = f(combinations(ii,:))-f0;
+                bestDirIndex = ii;
+            end
+        else
+            continue
+        end       
+    end
+
+end
+
+function combinations = generateNodeCombinations(depth)
+    % Recursion to get all the possible gradient direction vectors
+    % Initialize an empty cell array to store combinations
+    combinations = zeros(1,depth);
+
+    % Generate all possible combinations recursively
+    generateCombinationsRecursive(1, depth, []);
+
+    function generateCombinationsRecursive(currentDepth, targetDepth, currentCombination)
+        if currentDepth > targetDepth
+            % If reached the target depth, add the current combination to the list
+            combinations(end+1,:) = currentCombination;
+        else
+            % Generate combinations recursively for left and right children
+            generateCombinationsRecursive(currentDepth + 1, targetDepth, [currentCombination, 1]);
+            generateCombinationsRecursive(currentDepth + 1, targetDepth, [currentCombination, -1]);
+            generateCombinationsRecursive(currentDepth + 1, targetDepth, [currentCombination, 0]);
+        end
+    end
+
+    combinations = combinations(2:end,:);
+end
 end
 
 
--- test.m
+++ test.m
@@ -2,12 +2,11 @@ clear all; clc;
 
 syms f(x,y,z)
 
-x0 = [1,1];
-
-[x, err] = GDN(@test1, x0, 1)
+x0 = [1,0.5];
+[sol] = GDN_V2(@test1, @limits, x0, 1, 1e-10)
 
 x0 = [1,0];
-[x, err] = GDN(@test2, x0, 1, 1e-20, 20, 5, 2)
+[sol] = GDN_V2(@test2, @limits, x0, 1, 1e-10)
 
 function out = test1(vec)
     out = vec(1)^2+vec(2)^2; % sol : f[0,0]=0
@@ -19,4 +18,8 @@ function out = test2(vec)
     y = vec(2);
     out = (1+(x+y+1)^2*(19-14*x+3*x^2-14*y+6*x*y+3*y^2))*...
             (30+(2*x-3*y)^2*(18-32*x+12*x^2+48*y-36*x*y+27*y^2));
+end
+
+function out = limits(vec)
+    out = all([vec(1)<3, vec(2)<3, vec(1)>-3, vec(2)>-3]);
 end
